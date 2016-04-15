ce1e71bec06449fa907beb5f57aa11c04f82784d
Felix Engelbrektsson <ugglan00@gmail.com>
2016-04-15 13:51:38 +0200

    Testning utav character samt små ändringar i dess funktioner

---
+++ src/test/java/edu/chalmers/vaporwave/model/CharacterTest.java
@@ -0,0 +1,31 @@
+package edu.chalmers.vaporwave.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by FEngelbrektsson on 15/04/16.
+ */
+public class CharacterTest {
+
+    @Test
+    public void testMoveUp() throws Exception {
+
+    }
+
+    @Test
+    public void testMoveDown() throws Exception {
+
+    }
+
+    @Test
+    public void testMoveLeft() throws Exception {
+
+    }
+
+    @Test
+    public void testMoveRight() throws Exception {
+
+    }
+}
