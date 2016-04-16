2e5e282936510e511a166bf90ad400501fb2ba26
Lina <bllina@student.chalmers.se>
2016-04-16 13:38:09 +0200

    src/main/java/edu/chalmers/vaporwave/controller/ListenerController.java

--- src/main/java/edu/chalmers/vaporwave/model/menu/ArenaSelect.java
+++ src/main/java/edu/chalmers/vaporwave/model/menu/ArenaSelect.java
@@ -3,6 +3,6 @@ package edu.chalmers.vaporwave.model.menu;
 /**
  * Created by bob on 2016-04-15.
  */
-public class MapSelect implements MenuState {
+public class ArenaSelect implements MenuState {
 
 }
