e0ae978932901e7485ed95ff1a47052f23a03348
Felix Engelbrektsson <ugglan00@gmail.com>
2016-04-18 17:20:03 +0200

    Working on the powerups

---
+++ src/main/java/edu/chalmers/vaporwave/model/gameObjects/PowerUpState.java
@@ -0,0 +1,10 @@
+package edu.chalmers.vaporwave.model.gameObjects;
+
+/**
+ * Created by FEngelbrektsson on 18/04/16.
+ */
+public interface PowerUpState {
+    public int getRandomValue();
+    public int getEffect();
+    public void getPowerType;
+}
