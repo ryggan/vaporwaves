a0a7bb443e95e2b750ac46ef36e4061702ca1d08
Lina <bllina@student.chalmers.se>
2016-04-18 16:38:56 +0200

    Editing player class

--- src/main/java/edu/chalmers/vaporwave/model/Player.java
+++ src/main/java/edu/chalmers/vaporwave/model/Player.java
@@ -6,10 +6,38 @@ package edu.chalmers.vaporwave.model;
 public class Player {
     private int playerId;
     private String playerName;
-    //namn id options
+    private Character character;
+    private int score;
+    //name id character
+    //ListenerController
+    //everything that should be saved
+    //score
 
     public Player(int id) {
         this.playerId = id;
+        this.score=0;
+    }
+
+    public Player(int id, String playerName) {
+        this.playerId = id;
+        this.playerName=playerName;
+        this.score=0;
+    }
+
+    public void setScore(int score){
+        this.score=score;
+    }
+
+    public void clearScore(){
+        this.score=0;
+    }
+
+    public void setCharacter(Character c){
+        character=c;
+    }
+
+    public void clearCharacter(){
+        this.character=null;
     }
 
     public int getId() {
---
+++ src/main/java/edu/chalmers/vaporwave/model/gameObjects/HighScore.java
@@ -0,0 +1,7 @@
+package edu.chalmers.vaporwave.model.gameObjects;
+
+/**
+ * Created by Lina on 4/18/2016.
+ */
+public class HighScore {
+}
