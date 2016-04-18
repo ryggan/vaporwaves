cad532341fa464f7c546fffe85261792f1edf008
bob <robin.lilius@gmail.com>
2016-04-18 16:13:28 +0200

    Fixat ArenaModel

---
+++ src/main/java/edu/chalmers/vaporwave/model/ArenaModel.java
@@ -0,0 +1,53 @@
+package edu.chalmers.vaporwave.model;
+
+import edu.chalmers.vaporwave.model.gameObjects.Tile;
+
+import java.util.ArrayList;
+
+/**
+ * Created by andreascarlsson on 2016-04-18.
+ *
+ * Contains all arena objects
+ *
+ */
+public class ArenaModel {
+
+    private ArrayList<Tile>[][] arena;
+
+    public ArenaModel(int width, int height) {
+        newArena(width, height);
+    }
+    public ArenaModel() {
+        newArena(0, 0);
+    }
+
+    public void newArena(int width, int height) {
+        arena = new ArrayList[width][height];
+
+        for (int i = 0; i < arena.length; i++) {
+            for (int j = 0; j < arena[0].length; j++) {
+                arena[i][j] = new ArrayList<Tile>();
+            }
+        }
+    }
+
+    public ArrayList<Tile>[][] getArena() {
+        return arena;
+    }
+
+    public void setTile(Tile tile, int posx, int posy) {
+        arena[posx][posy].add(tile);
+    }
+
+    public ArrayList<Tile> getTiles(int posx, int posy) {
+        return arena[posx][posy];
+    }
+
+    public boolean removeTile(Tile tile, int posx, int posy) {
+        if (arena[posx][posy].contains(tile)) {
+            arena[posx][posy].remove(tile);
+            return true;
+        }
+        return false;
+    }
+}
