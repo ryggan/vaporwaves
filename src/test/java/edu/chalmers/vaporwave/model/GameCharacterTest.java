f8082139acd880271cc317482bbdc6bc1b5ad958
bob <robin.lilius@gmail.com>
2016-04-19 10:31:17 +0200

    Added GameCharacter again...

--- src/test/java/edu/chalmers/vaporwave/model/GameCharacterTest.java
+++ src/test/java/edu/chalmers/vaporwave/model/GameCharacterTest.java
@@ -14,8 +14,8 @@ public class GameCharacterTest {
 
     @Test
     public void testMoveUp() throws Exception {
-        Point p = new Point(0,0);
-        GameCharacter ch = new GameCharacter(p);
+//        Point p = new Point(0,0);
+        GameCharacter ch = new GameCharacter();
         ch.moveUp();
         assertTrue(ch.getXPosition() == 0);
         assertTrue(ch.getYPosition() == 1);
@@ -23,8 +23,8 @@ public class GameCharacterTest {
 
     @Test
     public void testMoveDown() throws Exception {
-        Point p = new Point(0,0);
-        GameCharacter ch = new GameCharacter(p);
+//        Point p = new Point(0,0);
+        GameCharacter ch = new GameCharacter();
         ch.moveDown();
         assertTrue(ch.getXPosition() == 0);
         assertTrue(ch.getYPosition() == -1);
@@ -33,8 +33,8 @@ public class GameCharacterTest {
 
     @Test
     public void testMoveLeft() throws Exception {
-        Point p = new Point(0,0);
-        GameCharacter ch = new GameCharacter(p);
+//        Point p = new Point(0,0);
+        GameCharacter ch = new GameCharacter();
         ch.moveLeft();
         assertTrue(ch.getXPosition() == -1);
         assertTrue(ch.getYPosition() == 0);
@@ -43,8 +43,8 @@ public class GameCharacterTest {
 
     @Test
     public void testMoveRight() throws Exception {
-        Point p = new Point(0,0);
-        GameCharacter ch = new GameCharacter(p);
+//        Point p = new Point(0,0);
+        GameCharacter ch = new GameCharacter();
         ch.moveRight();
         assertTrue(ch.getXPosition() == 1);
         assertTrue(ch.getYPosition() == 0);
