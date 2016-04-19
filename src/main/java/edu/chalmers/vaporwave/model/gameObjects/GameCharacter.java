29afc0782dc24030f6a4754d2b621caff3c2742a
bob <robin.lilius@gmail.com>
2016-04-19 08:31:59 +0200

    Small changes to gameCharacter

--- src/main/java/edu/chalmers/vaporwave/model/gameObjects/GameCharacter.java
+++ src/main/java/edu/chalmers/vaporwave/model/gameObjects/GameCharacter.java
@@ -9,6 +9,7 @@ import javafx.scene.image.Image;
 import javafx.scene.input.KeyEvent;
 
 import java.awt.*;
+import java.io.File;
 
 
 /**
@@ -29,25 +30,26 @@ public class GameCharacter extends DynamicTile {
     public GameCharacter(Point position) {
         this.currentPosition = position;
 
+        Image spriteSheet0 = new Image("Images/spritesheet-alyssa-spawn-48x128.png");
         Image spriteSheet1 = new Image("Images/spritesheet-alyssa-walkidleflinch-48x32.png");
         Image spriteSheet2 = new Image("Images/spritesheet-alyssa-death-56x56.png");
 
+        spawnSprite = new AnimatedSprite(spriteSheet0, new Dimension(48, 128), 27, 0.1, new int[] {0, 0});
+        spawnSprite.setScale(Constants.GAME_SCALE);
         for (int i = 0; i < 4; i++) {
             idleSprite[i] = new AnimatedSprite(spriteSheet1, new Dimension(48, 32), 1, 0.1, new int[] {i, 4});
             idleSprite[i].setScale(Constants.GAME_SCALE);
-        }
-        for (int i = 0; i < 4; i++) {
+
             walkSprite[i] = new AnimatedSprite(spriteSheet1, new Dimension(48, 32), 8, 0.1, new int[] {0, i});
             walkSprite[i].setScale(Constants.GAME_SCALE);
-        }
-        for (int i = 0; i < 4; i++) {
+
             flinchSprite[i] = new AnimatedSprite(spriteSheet1, new Dimension(48, 32), 1, 0.1, new int[] {4+i, 4});
             flinchSprite[i].setScale(Constants.GAME_SCALE);
         }
         deathSprite = new AnimatedSprite(spriteSheet2, new Dimension(56, 56), 28, 0.1, new int[] {0, 0});
         deathSprite.setScale(Constants.GAME_SCALE);
 
-        setSprite(idleSprite[0]);
+        setSprite(walkSprite[2]);
 
     }
 
