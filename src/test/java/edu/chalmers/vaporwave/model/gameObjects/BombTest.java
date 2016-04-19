e655a9149b0053b35a6a20621e5a78d317196a09
ryggan <andreas@carlssonfoto.se>
2016-04-19 16:40:12 +0200

    Test subscribe method in BombTest

--- src/test/java/edu/chalmers/vaporwave/model/gameObjects/BombTest.java
+++ src/test/java/edu/chalmers/vaporwave/model/gameObjects/BombTest.java
@@ -5,6 +5,7 @@ import edu.chalmers.vaporwave.event.IEvent;
 import edu.chalmers.vaporwave.event.IEventListener;
 import edu.chalmers.vaporwave.model.gameObjects.Bomb;
 import edu.chalmers.*;
+import org.junit.Test;
 
 
 import static org.junit.Assert.*;
@@ -12,18 +13,23 @@ import static org.junit.Assert.*;
 /**
  * Created by FEngelbrektsson on 19/04/16.
  */
-public class BombTest /*implements IEventListener*/ {
+public class BombTest implements IEventListener {
 
-    /**
-     * Put a pin in this fucking test
-     */
-/*    EventBus.BUS.subscribe(this);
-    Bomb bomb = new Bomb(10, 3);
+    @Test
+    public void testSubscrive() {
+        /**
+         * Put a pin in this fucking test
+         */
+        EventBus.BUS.subscribe(this);
+        Bomb bomb = new Bomb(10, 3);
 
-    eventFired(bomb);
+        eventFired(bomb);
+
+
+    }
 
     public void eventFired(IEvent event) {
         assertTrue(bomb.fireEvent() == true);
-    }*/
+    }
 
 }
