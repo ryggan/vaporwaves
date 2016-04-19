07def3e4c0ffc4d97cc4bf49efdea4f5011ea06f
Lina <bllina@student.chalmers.se>
2016-04-19 15:09:59 +0200

    characterselect

--- src/main/java/edu/chalmers/vaporwave/model/menu/CharacterSelect.java
+++ src/main/java/edu/chalmers/vaporwave/model/menu/CharacterSelect.java
@@ -1,10 +1,13 @@
 package edu.chalmers.vaporwave.model.menu;
 
+import javax.swing.text.html.ImageView;
+
 /**
  * Created by bob on 2016-04-15.
  */
 public class CharacterSelect implements MenuState {
 
-
+    ImageView[] characters;
+    ImageView[] 
     
 }
