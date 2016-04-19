554ad6cb312faf3257002456cc87358b0bc5a52b
Felix Engelbrektsson <ugglan00@gmail.com>
2016-04-19 16:23:15 +0200

    Implemented IEvent to bomb, and made a non-functioning bombtest

--- src/main/java/edu/chalmers/vaporwave/model/gameObjects/Bomb.java
+++ src/main/java/edu/chalmers/vaporwave/model/gameObjects/Bomb.java
@@ -1,5 +1,6 @@
 package edu.chalmers.vaporwave.model.gameObjects;
 
+import edu.chalmers.vaporwave.event.IEvent;
 import edu.chalmers.vaporwave.view.Sprite;
 
 import java.awt.*;
@@ -9,11 +10,16 @@ import java.util.TimerTask;
 /**
  * Created by FEngelbrektsson on 15/04/16.
  */
-public class Bomb extends Explosive{
+public class Bomb extends Explosive implements IEvent {
 
     private int delay;
     //timer
 
+
+    //Constructor for JUnit tester
+    public Bomb(int range, int delay) {
+        this.delay = delay;
+    }
     public Bomb(Sprite s, Point cPos, Point gPos, int range, int delay){
         super(s, cPos, gPos, range);
         this.delay=delay;
@@ -30,6 +36,11 @@ public class Bomb extends Explosive{
 
     public Bomb getOuter() {
         return Bomb.this;
+    }
+
+    //Made fireEvent return boolean for testing purposes
+    public void fireEvent() {
+        startTimer();
     }
 
 
