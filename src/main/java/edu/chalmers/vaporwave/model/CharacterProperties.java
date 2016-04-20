1c6410ada36de1d9f006d7548c1ab81f4db0e0e6
ryggan <andreas@carlssonfoto.se>
2016-04-20 14:51:15 +0200

    Adding spritesheets to spriteProperties and updating CharactherLoader

--- src/main/java/edu/chalmers/vaporwave/model/CharacterProperties.java
+++ src/main/java/edu/chalmers/vaporwave/model/CharacterProperties.java
@@ -1,19 +1,31 @@
 package edu.chalmers.vaporwave.model;
 
+import edu.chalmers.vaporwave.util.State;
+
+import java.util.Map;
+
 /**
  * Created by andreascarlsson on 2016-04-20.
  */
 public class CharacterProperties {
     private String name;
-    private SpriteProperties[] spriteProperties;
+    private Map<State, SpriteProperties> spriteProperties;
 
-    public CharacterProperties(String name, SpriteProperties[] spriteProperties) {
+    public CharacterProperties(String name, Map<State, SpriteProperties> spriteProperties) {
         this.name = name;
-        this.spriteProperties[] = spriteProperties;
+        this.spriteProperties = spriteProperties;
     }
 
     public String getName() {
         return this.name;
+    }
+
+    public Map getSpriteProperties() {
+        return this.spriteProperties;
+    }
+
+    public SpriteProperties getSpriteProperties(State state) {
+        return this.spriteProperties.get(state);
     }
 
 }
--- src/main/java/edu/chalmers/vaporwave/util/CharacterLoader.java
+++ src/main/java/edu/chalmers/vaporwave/util/CharacterLoader.java
@@ -1,51 +1,57 @@
 package edu.chalmers.vaporwave.util;
 
+import edu.chalmers.vaporwave.model.CharacterProperties;
 import edu.chalmers.vaporwave.model.SpriteProperties;
+import javafx.scene.image.Image;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by andreascarlsson on 2016-04-19.
  */
 public class CharacterLoader {
 
-    public static SpriteProperties[] loadCharacter(NodeList nodeList, String name) {
+    public static CharacterProperties loadCharacter(NodeList nodeList, String name) {
 
-
-        SpriteProperties spriteProperties[] = new SpriteProperties[nodeList.getLength()];
-
-        System.out.println("sprite");
+        Map<State, SpriteProperties> spritePropertiesMap = new HashMap<State, SpriteProperties>();
 
         for (int i = 0; i < nodeList.getLength(); i++) {
 
             Element allCharacterNodes = (Element)nodeList.item(i);
 
             for (int j = 0; j < allCharacterNodes.getElementsByTagName("character").getLength(); j++) {
-                Element singleCharacterNodes = (Element)nodeList.item(i);
+                Element singleCharacterNodes = (Element)allCharacterNodes;
                 String currentName = singleCharacterNodes.getElementsByTagName("name").item(j).getTextContent();
                 if (currentName.equals(name)) {
-                    System.out.println("Fetching info for: " + name);
 
+                    for (int k = 0; k < State.values().length - 1; k++) {
+
+                        Element currentSpriteNodes = (Element)singleCharacterNodes.getElementsByTagName(Constants.CHARACTER_STATE[k].toString().toLowerCase()).item(j);
+
+                        Image spritesheet = new Image(currentSpriteNodes.getElementsByTagName("spritesheet").item(0).getTextContent());
+                        int xDimension = Integer.parseInt(currentSpriteNodes.getElementsByTagName("xDimension").item(0).getTextContent());
+                        int yDimension = Integer.parseInt(currentSpriteNodes.getElementsByTagName("yDimension").item(0).getTextContent());
+                        int frames = Integer.parseInt(currentSpriteNodes.getElementsByTagName("frames").item(0).getTextContent());
+                        double duration = Double.parseDouble(currentSpriteNodes.getElementsByTagName("duration").item(0).getTextContent());
+                        int[] firstFrame = { Integer.parseInt(currentSpriteNodes.getElementsByTagName("firstFrameX").item(0).getTextContent()),
+                                Integer.parseInt(currentSpriteNodes.getElementsByTagName("firstFrameY").item(0).getTextContent())};
+
+                        SpriteProperties property = new SpriteProperties(Constants.CHARACTER_STATE[k].toString().toLowerCase(),
+                                spritesheet, xDimension, yDimension, frames, duration, firstFrame);
+                        spritePropertiesMap.put(Constants.CHARACTER_STATE[k], property);
+
+                    }
 
                 }
 
             }
 
-//            String state = element.getElementsByTagName("state").item(0).getTextContent();
-//            int xDimension = Integer.parseInt(element.getElementsByTagName("xDimension").item(0).getTextContent());
-//            int yDimension = Integer.parseInt(element.getElementsByTagName("yDimension").item(0).getTextContent());
-//            int frames = Integer.parseInt(element.getElementsByTagName("frames").item(0).getTextContent());
-//            double duration = Double.parseDouble(element.getElementsByTagName("duration").item(0).getTextContent());
-//            int[] firstFrame = { Integer.parseInt(element.getElementsByTagName("firstFrameX").item(0).getTextContent()), Integer.parseInt(element.getElementsByTagName("firstFrameY").item(0).getTextContent())};
-//
-//            SpriteProperties property = new SpriteProperties(state, xDimension, yDimension, frames, duration, firstFrame);
-//            spriteProperties[i] = property;
-
-
         }
 
-
-        return spriteProperties;
+        return new CharacterProperties(name, spritePropertiesMap);
 
     }
 
