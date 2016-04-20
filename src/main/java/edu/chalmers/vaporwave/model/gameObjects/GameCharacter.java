57bd22edf83e0b310900c0c03250eb5d1c35873c
bob <robin.lilius@gmail.com>
2016-04-20 13:59:59 +0200

    Added states and spritehandling accordingly to GameCharacter

--- src/main/java/edu/chalmers/vaporwave/model/gameObjects/GameCharacter.java
+++ src/main/java/edu/chalmers/vaporwave/model/gameObjects/GameCharacter.java
@@ -21,12 +21,20 @@ public class GameCharacter extends DynamicTile {
     private Player player;
     private int playerId;
     private Directions direction;
+//    private boolean moving;
+    private String characterState;
 
-    private Sprite spawnSprite;
+    private double maxHealth;
+    private double health;
+    private double speed;
+    private int bombRange;
+    private int bombCount;
+
+    private Sprite[] spawnSprite = new Sprite[1];
     private Sprite[] idleSprite = new Sprite[4];
     private Sprite[] walkSprite = new Sprite[4];
     private Sprite[] flinchSprite = new Sprite[4];
-    private Sprite deathSprite;
+    private Sprite[] deathSprite = new Sprite[1];
 
     public GameCharacter(String name) {
 
@@ -42,8 +50,8 @@ public class GameCharacter extends DynamicTile {
         Image spriteSheet1 = new Image("images/spritesheet-alyssa-walkidleflinch-48x48.png");
         Image spriteSheet2 = new Image("images/spritesheet-alyssa-death-56x56.png");
 
-        spawnSprite = new AnimatedSprite(spriteSheet0, new Dimension(48, 128), 27, 0.1, new int[] {0, 0});
-        spawnSprite.setScale(Constants.GAME_SCALE);
+        spawnSprite[0] = new AnimatedSprite(spriteSheet0, new Dimension(48, 128), 27, 0.1, new int[] {0, 0});
+        spawnSprite[0].setScale(Constants.GAME_SCALE);
         for (int i = 0; i < 4; i++) {
             idleSprite[i] = new AnimatedSprite(spriteSheet1, new Dimension(48, 48), 1, 0.1, new int[] {i, 4});
             idleSprite[i].setScale(Constants.GAME_SCALE);
@@ -54,72 +62,101 @@ public class GameCharacter extends DynamicTile {
             flinchSprite[i] = new AnimatedSprite(spriteSheet1, new Dimension(48, 48), 1, 0.1, new int[] {4+i, 4});
             flinchSprite[i].setScale(Constants.GAME_SCALE);
         }
-        deathSprite = new AnimatedSprite(spriteSheet2, new Dimension(56, 56), 28, 0.1, new int[] {0, 0});
-        deathSprite.setScale(Constants.GAME_SCALE);
+        deathSprite[0] = new AnimatedSprite(spriteSheet2, new Dimension(56, 56), 28, 0.1, new int[] {0, 0});
+        deathSprite[0].setScale(Constants.GAME_SCALE);
 
         setGridPosition(new Point(0,0));
-        moveRight();
-
+        characterState = "IDLE";
+        direction = Directions.DOWN;
+        updateSprite();
     }
 
-
-    // Should be rewritten
-    public GameCharacter(Point position, Player player) {
-        this.currentGridPosition = position;
-        this.player = player;
-        this.playerId = player.getId();
-    }
-
-    // Change this to use enum instead
     public void move(String key) {
-        if (key.equals("UP")) {
-            direction = Directions.UP;
-            moveUp();
-        } else if (key.equals("LEFT")) {
-            direction = Directions.LEFT;
-            moveLeft();
-        } else if (key.equals("DOWN")) {
-            direction = Directions.DOWN;
-            moveDown();
-        } else if (key.equals("RIGHT")) {
-            direction = Directions.RIGHT;
-            moveRight();
+//        if (!characterState.equals("WALK") || oppositeDirection(key)) {
+            characterState = "WALK";
+            if (key.equals("UP")) {
+                moveUp();
+            } else if (key.equals("LEFT")) {
+                moveLeft();
+            } else if (key.equals("DOWN")) {
+                moveDown();
+            } else if (key.equals("RIGHT")) {
+                moveRight();
+            }
+            updateSprite();
+//        }
+    }
+
+    private boolean oppositeDirection(String key) {
+        return (key.equals("UP") && direction == Directions.DOWN)
+                || (key.equals("DOWN") && direction == Directions.UP)
+                || (key.equals("RIGHT") && direction == Directions.LEFT)
+                || (key.equals("LEFT") && direction == Directions.RIGHT);
+    }
+
+    private void updateSprite() {
+        Sprite[] currentSprite = idleSprite; // Always idle if no other state is active
+        if (characterState.equals("WALK")) {
+            currentSprite = walkSprite;
+        } else if (characterState.equals("FLINCH")) {
+            currentSprite = flinchSprite;
+        } else if (characterState.equals("SPAWN")) {
+            currentSprite = spawnSprite;
+        } else if (characterState.equals("DEATH")) {
+            currentSprite = deathSprite;
         }
+
+        int spriteIndex;
+        if (direction == Directions.LEFT) {
+            spriteIndex = 1;
+        } else if (direction == Directions.RIGHT) {
+            spriteIndex = 2;
+        } else if (direction == Directions.UP) {
+            spriteIndex = 3;
+        } else {
+            spriteIndex = 0; // Happens when direction = DOWN, or when characterState = SPAWN/DEATH
+        }
+        setSprite(currentSprite[spriteIndex]);
     }
 
     public void moveUp() {
-        previousGridPosition = currentGridPosition;
-        currentGridPosition.setLocation(previousGridPosition.getX(), previousGridPosition.getY() + 1);
-        setSprite(walkSprite[3]);
+        direction = Directions.UP;
+
+//        previousGridPosition = currentGridPosition;
+//        currentGridPosition.setLocation(previousGridPosition.getX(), previousGridPosition.getY() + 1);
+//        setSprite(walkSprite[3]);
     }
     public void moveDown() {
-        previousGridPosition = currentGridPosition;
-        currentGridPosition.setLocation(previousGridPosition.getX(), previousGridPosition.getY() - 1);
-        setSprite(walkSprite[0]);
+        direction = Directions.DOWN;
+//        previousGridPosition = currentGridPosition;
+//        currentGridPosition.setLocation(previousGridPosition.getX(), previousGridPosition.getY() - 1);
+//        setSprite(walkSprite[0]);
     }
     public void moveLeft() {
-        previousGridPosition = currentGridPosition;
-        currentGridPosition.setLocation(previousGridPosition.getX() - 1, previousGridPosition.getY());
-        setSprite(walkSprite[1]);
+        direction = Directions.LEFT;
+//        previousGridPosition = currentGridPosition;
+//        currentGridPosition.setLocation(previousGridPosition.getX() - 1, previousGridPosition.getY());
+//        setSprite(walkSprite[1]);
     }
     public void moveRight() {
-        previousGridPosition = currentGridPosition;
-        currentGridPosition.setLocation(previousGridPosition.getX() + 1, previousGridPosition.getY());
-        setSprite(walkSprite[2]);
+        direction = Directions.RIGHT;
+//        previousGridPosition = currentGridPosition;
+//        currentGridPosition.setLocation(previousGridPosition.getX() + 1, previousGridPosition.getY());
+//        setSprite(walkSprite[2]);
     }
 
-    public void setGridPosition(Point gridPosition) {
-        this.currentGridPosition = gridPosition;
-        this.previousGridPosition = gridPosition;
-    }
-
-    public Point getGridPosition() {
-        return currentGridPosition;
-    }
-    public double getXPosition() {
-        return currentGridPosition.getX();
-    }
-    public double getYPosition() {
-        return currentGridPosition.getY();
-    }
+//    public void setGridPosition(Point gridPosition) {
+//        this.currentGridPosition = gridPosition;
+//        this.previousGridPosition = gridPosition;
+//    }
+//
+//    public Point getGridPosition() {
+//        return currentGridPosition;
+//    }
+//    public double getXPosition() {
+//        return currentGridPosition.getX();
+//    }
+//    public double getYPosition() {
+//        return currentGridPosition.getY();
+//    }
 }
