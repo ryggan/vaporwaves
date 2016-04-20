caa43cfde1bb53db4a2c97c594eae90b41d54bab
Lina <bllina@student.chalmers.se>
2016-04-20 18:04:36 +0200

    menu

--- src/main/java/edu/chalmers/vaporwave/model/menu/StartMenu.java
+++ src/main/java/edu/chalmers/vaporwave/model/menu/StartMenu.java
@@ -1,8 +1,13 @@
 package edu.chalmers.vaporwave.model.menu;
 
+import javafx.scene.image.ImageView;
+
 /**
  * Created by bob on 2016-04-15.
  */
 public class StartMenu implements MenuState {
+
+    ImageView startGame;
+
 
 }
