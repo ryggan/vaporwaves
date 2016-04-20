e2132f4155a1b1b92ee52e50068210a921e833dd
ryggan <andreas@carlssonfoto.se>
2016-04-20 09:10:01 +0200

    Iterating through Character XML

--- src/main/java/edu/chalmers/vaporwave/util/CharacterLoader.java
+++ src/main/java/edu/chalmers/vaporwave/util/CharacterLoader.java
@@ -9,23 +9,37 @@ import org.w3c.dom.NodeList;
  */
 public class CharacterLoader {
 
-    public static SpriteProperties[] loadCharacters(NodeList nodeList) {
+    public static SpriteProperties[] loadCharacter(NodeList nodeList, String name) {
 
 
         SpriteProperties spriteProperties[] = new SpriteProperties[nodeList.getLength()];
 
+        System.out.println("sprite");
+
         for (int i = 0; i < nodeList.getLength(); i++) {
-            Element element = (Element)nodeList.item(i);
 
-            String state = element.getElementsByTagName("state").item(0).getTextContent();
-            int xDimension = Integer.parseInt(element.getElementsByTagName("xDimension").item(0).getTextContent());
-            int yDimension = Integer.parseInt(element.getElementsByTagName("yDimension").item(0).getTextContent());
-            int frames = Integer.parseInt(element.getElementsByTagName("frames").item(0).getTextContent());
-            double duration = Double.parseDouble(element.getElementsByTagName("duration").item(0).getTextContent());
-            int[] firstFrame = { Integer.parseInt(element.getElementsByTagName("firstFrameX").item(0).getTextContent()), Integer.parseInt(element.getElementsByTagName("firstFrameY").item(0).getTextContent())};
+            Element allCharacterNodes = (Element)nodeList.item(i);
 
-            SpriteProperties property = new SpriteProperties(state, xDimension, yDimension, frames, duration, firstFrame);
-            spriteProperties[i] = property;
+            for (int j = 0; j < allCharacterNodes.getElementsByTagName("character").getLength(); j++) {
+                Element singleCharacterNodes = (Element)nodeList.item(i);
+                String currentName = singleCharacterNodes.getElementsByTagName("name").item(j).getTextContent();
+                if (currentName.equals(name)) {
+                    System.out.println("Fetching info for: " + name);
+
+
+                }
+
+            }
+
+//            String state = element.getElementsByTagName("state").item(0).getTextContent();
+//            int xDimension = Integer.parseInt(element.getElementsByTagName("xDimension").item(0).getTextContent());
+//            int yDimension = Integer.parseInt(element.getElementsByTagName("yDimension").item(0).getTextContent());
+//            int frames = Integer.parseInt(element.getElementsByTagName("frames").item(0).getTextContent());
+//            double duration = Double.parseDouble(element.getElementsByTagName("duration").item(0).getTextContent());
+//            int[] firstFrame = { Integer.parseInt(element.getElementsByTagName("firstFrameX").item(0).getTextContent()), Integer.parseInt(element.getElementsByTagName("firstFrameY").item(0).getTextContent())};
+//
+//            SpriteProperties property = new SpriteProperties(state, xDimension, yDimension, frames, duration, firstFrame);
+//            spriteProperties[i] = property;
 
 
         }
