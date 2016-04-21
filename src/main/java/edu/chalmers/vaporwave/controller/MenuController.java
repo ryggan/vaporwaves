2ede5065651df6faf7c69bbd1ded11ed00882f03
ryggan <andreas@carlssonfoto.se>
2016-04-21 10:00:11 +0200

    Starts working on MenuController. GameController will listen on MenuController

--- src/main/java/edu/chalmers/vaporwave/controller/MenuController.java
+++ src/main/java/edu/chalmers/vaporwave/controller/MenuController.java
@@ -5,27 +5,22 @@ import javafx.scene.Group;
 
 import java.util.ArrayList;
 
-/**
- * Created by bob on 2016-04-15.
- */
 public class MenuController {
 
-    private MenuView mv;
+    private MenuView menuView;
 
     public MenuController(Group root) {
-        mv = new MenuView(root);
+        menuView = new MenuView(root);
     }
 
     public void timerUpdate(double timeSinceStart, double timeSinceLastCall, ArrayList<String> input) {
 
         for (int i = 0; i < input.size(); i++) {
             String key = input.get(i);
-            if (key.equals("UP") || key.equals("DOWN"))
-                mv.changeSelected(key);
+            if (key.equals("UP") || key.equals("DOWN")) {
+                menuView.changeSelected(key);
+            }
         }
-        
-//        av.updateView(timeSinceStart);
-    } //menustate
 
-
+    }
 }
