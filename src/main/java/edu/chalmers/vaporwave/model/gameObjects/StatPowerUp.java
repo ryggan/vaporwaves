e56b9668ba7aa1c80db122bfc853d0ad3779afbe
ryggan <andreas@carlssonfoto.se>
2016-04-22 08:43:09 +0200

    Refactoring PowerUp system to use state pattern

--- src/main/java/edu/chalmers/vaporwave/model/gameObjects/StatPowerUp.java
+++ src/main/java/edu/chalmers/vaporwave/model/gameObjects/StatPowerUp.java
@@ -2,29 +2,27 @@ package edu.chalmers.vaporwave.model.gameObjects;
 
 import edu.chalmers.vaporwave.util.PowerUpState;
 
-import java.util.List;
-import java.util.Random;
-
 public class StatPowerUp extends PowerUp {
-    private PowerUpState powerUpState;
 
-    public StatPowerUp(List<PowerUp> enabledPowerUpList) {
+    int spawnChance;
+    int statusEffect;
+    PowerUpState powerUpState;
 
-        if(enabledPowerUpList.size() > 0) {
-            int randomMaxValue = 0;
-            for (int i = 0; i < enabledPowerUpList.size(); i++) {
-                randomMaxValue = randomMaxValue + enabledPowerUpList.get(i).getSpawnChance();
-            }
+    public StatPowerUp(int spawnChance, int statusEffect, PowerUpState powerUpState) {
+        this.spawnChance = spawnChance;
+        this.statusEffect = statusEffect;
+        this.powerUpState = powerUpState;
+    }
 
-            Random r = new Random();
-            int randomValue = r.nextInt(randomMaxValue);
-            int j = 0;
-            for (int i = 0; i < enabledPowerUpList.size(); i++) {
-                j = j + enabledPowerUpList.get(i).getSpawnChance();
-                if(j <= randomValue) {
-//                    powerUpState = enabledPowerUpList.get(i);
-                }
-            }
-        }
+    public int getSpawnChance() {
+        return spawnChance;
+    }
+
+    public int getStatusEffect() {
+        return statusEffect;
+    }
+
+    public PowerUpState getPowerType() {
+        return powerUpState;
     }
 }
