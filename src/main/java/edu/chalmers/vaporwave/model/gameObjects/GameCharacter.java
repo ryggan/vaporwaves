a83ad8b7d97922994f076d21023e15a216bb5538
ryggan <andreas@carlssonfoto.se>
2016-04-25 13:29:17 +0200

    Refactored Movable and GameCharacter. Put common code in GameCharacter into Movable to make it more generalized.

--- src/main/java/edu/chalmers/vaporwave/model/gameObjects/GameCharacter.java
+++ src/main/java/edu/chalmers/vaporwave/model/gameObjects/GameCharacter.java
@@ -1,5 +1,6 @@
 package edu.chalmers.vaporwave.model.gameObjects;
 
+import com.sun.javafx.scene.traversal.Direction;
 import edu.chalmers.vaporwave.controller.ListenerController;
 import edu.chalmers.vaporwave.event.GameEventBus;
 import edu.chalmers.vaporwave.event.PlaceBombEvent;
@@ -13,192 +14,42 @@ public class GameCharacter extends Movable {
 
     private String name;
 
-    private Player player;
-    private int playerId;
-
-    private Directions direction;
-    private CharacterState characterState;
-    private int previousGridPositionX;
-    private int previousGridPositionY;
-    private boolean moving;
-    private StaticTile[][] latestArenaTiles;
-
     private double maxHealth;
     private double health;
-    private double speed;
     private int bombRange;
     private int bombCount;
 
     public GameCharacter(String name) {
+        super(Utils.gridToCanvasPosition(6), Utils.gridToCanvasPosition(5), 0.8);
 
-        // Test settings setup:
-        setCanvasPosition(Utils.gridToCanvasPosition(6), Utils.gridToCanvasPosition(5));
-        this.speed = 0.8;
         this.bombRange = 2;
         this.bombCount = 5;
+        this.maxHealth = 100.0;
         this.health = 100.0;
-
-        // Setup:
         this.name = name;
-        this.moving = false;
-        this.characterState = CharacterState.IDLE;
-        this.direction = Directions.DOWN;
-        this.previousGridPositionX = Utils.canvasToGridPosition(getCanvasPositionX());
-        this.previousGridPositionY = Utils.canvasToGridPosition(getCanvasPositionY());
     }
 
     public void placeBomb() {
         if(this.bombCount > 0) {
-            GameEventBus.getInstance().post(new PlaceBombEvent(Utils.canvasToGridPosition(this.getCanvasPositionX(), this.getCanvasPositionY()), bombRange));
+            GameEventBus.getInstance().post(new PlaceBombEvent(
+                            Utils.canvasToGridPosition(this.getCanvasPositionX(), this.getCanvasPositionY()
+                            ), bombRange)
+                    );
             this.bombCount -= 1;
         }
-    }
-
-    public void move(String key, StaticTile[][] arenaTiles) {
-        this.latestArenaTiles = arenaTiles;
-        if (characterState != CharacterState.WALK || oppositeDirection(key)) {
-            switch (key) {
-                case "UP":
-                    moveUp();
-                    break;
-                case "LEFT":
-                    moveLeft();
-                    break;
-                case "DOWN":
-                    moveDown();
-                    break;
-                case "RIGHT":
-                    moveRight();
-                    break;
-            }
-            if (getVelocityY() != 0 || getVelocityX() != 0) {
-                characterState = CharacterState.WALK;
-            }
-        }
-    }
-
-    private boolean oppositeDirection(String key) {
-        return (key.equals("UP") && direction == Directions.DOWN)
-                || (key.equals("DOWN") && direction == Directions.UP)
-                || (key.equals("RIGHT") && direction == Directions.LEFT)
-                || (key.equals("LEFT") && direction == Directions.RIGHT);
-    }
-
-    public void moveUp() {
-        direction = Directions.UP;
-        if (allowMove(0, -1) && getCanvasPositionY() > 0) {
-            setVelocity(0, -this.speed);
-        }
-    }
-    public void moveDown() {
-        direction = Directions.DOWN;
-        if (allowMove(0, 1) && getCanvasPositionY() < Utils.gridToCanvasPosition(Constants.DEFAULT_GRID_HEIGHT-1)) {
-            setVelocity(0, this.speed);
-        }
-    }
-    public void moveLeft() {
-        direction = Directions.LEFT;
-        if (allowMove(-1, 0) && getCanvasPositionX() > 0) {
-            setVelocity(-this.speed, 0);
-        }
-    }
-    public void moveRight() {
-        direction = Directions.RIGHT;
-        if (allowMove(1, 0) && getCanvasPositionX() < Utils.gridToCanvasPosition(Constants.DEFAULT_GRID_WIDTH-1)) {
-            setVelocity(this.speed, 0);
-        }
-    }
-
-    private boolean allowMove(int gridDirectionX, int gridDirectionY) {
-        int nextGridPositionX = Math.min(Math.max(previousGridPositionX + gridDirectionX, 0), Constants.DEFAULT_GRID_WIDTH-1);
-        int nextGridPositionY = Math.min(Math.max(previousGridPositionY + gridDirectionY, 0), Constants.DEFAULT_GRID_HEIGHT-1);
-        StaticTile nextTile = this.latestArenaTiles[nextGridPositionX][nextGridPositionY];
-
-        return nextTile == null || !(nextTile instanceof Wall) && !(nextTile instanceof Bomb);
-    }
-
-    @Override
-    public void updatePosition() {
-        super.updatePosition();
-        if (characterState == CharacterState.WALK) {
-            stopOnTileIfNeeded();
-        }
-        moving = (getVelocityX() != 0 || getVelocityY() != 0);
-    }
-
-    private void stopOnTileIfNeeded() {
-        int closestTilePositionX = Utils.canvasToGridPosition(getCanvasPositionX());
-        int closestTilePositionY = Utils.canvasToGridPosition(getCanvasPositionY());
-
-        double compareX = Math.abs(Utils.gridToCanvasPosition(closestTilePositionX) - getCanvasPositionX());
-        double compareY = Math.abs(Utils.gridToCanvasPosition(closestTilePositionY) - getCanvasPositionY());
-
-        boolean closeToPosition = (compareX <= this.speed / 2) && (compareY <= this.speed / 2)
-                && (moving || (closestTilePositionX != previousGridPositionX || closestTilePositionY != previousGridPositionY));
-
-        if(closeToPosition) {
-            stop(closestTilePositionX, closestTilePositionY);
-        }
-    }
-
-    private void stop(int newGridPositionX, int newGridPositionY) {
-
-        characterState = CharacterState.IDLE;
-        setVelocity(0, 0);
-        setCanvasPosition(Utils.gridToCanvasPosition(newGridPositionX), Utils.gridToCanvasPosition(newGridPositionY));
-        previousGridPositionX = newGridPositionX;
-        previousGridPositionY = newGridPositionY;
-
-        List<String> input = ListenerController.getInstance().getInput();
-        if (input.size() > 0 && (input.contains("UP") || input.contains("DOWN") || input.contains("LEFT") || input.contains("RIGHT"))) {
-            move(input.get(input.size()-1), this.latestArenaTiles);
-        }
-    }
-
-    public void death() {
-        if (characterState == CharacterState.IDLE) {
-            characterState = CharacterState.DEATH;
-        }
-    }
-
-    public void spawn() {
-        if (characterState == CharacterState.IDLE) {
-            characterState = CharacterState.SPAWN;
-        }
-    }
-
-    // GETS AND SETS:
-
-    public Point getGridPosition() {
-        return new Point(Utils.canvasToGridPosition(this.getCanvasPositionX(), this.getCanvasPositionY()));
-    }
-
-    public CharacterState getState() {
-        return this.characterState;
     }
 
     public String getName() {
         return this.name;
     }
 
-    public Directions getDirection() {
-        return direction;
+
+    public void setMaxHealth(double maxHealth) {
+        this.maxHealth = maxHealth;
     }
 
-    public double getSpeed() {
-        return this.speed;
-    }
-
-    public void setSpeed(double speed) {
-        this.speed = speed;
-    }
-
-    public int getBombCount() {
-        return this.bombCount;
-    }
-
-    public void setBombCount(int bombCount) {
-        this.bombCount = bombCount;
+    public double getMaxHealth() {
+        return this.maxHealth;
     }
 
     public double getHealth() {
@@ -215,5 +66,13 @@ public class GameCharacter extends Movable {
 
     public void setBombRange(int bombRange) {
         this.bombRange = bombRange;
+    }
+
+    public int getBombCount() {
+        return this.bombCount;
+    }
+
+    public void setBombCount(int bombCount) {
+        this.bombCount = bombCount;
     }
 }
--- src/main/java/edu/chalmers/vaporwave/model/gameObjects/Movable.java
+++ src/main/java/edu/chalmers/vaporwave/model/gameObjects/Movable.java
@@ -1,36 +1,90 @@
 package edu.chalmers.vaporwave.model.gameObjects;
 
+import com.sun.javafx.scene.traversal.Direction;
+import edu.chalmers.vaporwave.controller.ListenerController;
+import edu.chalmers.vaporwave.util.CharacterState;
 import edu.chalmers.vaporwave.util.Constants;
+import edu.chalmers.vaporwave.util.Utils;
 import edu.chalmers.vaporwave.view.Sprite;
 import javafx.scene.canvas.GraphicsContext;
 
 import java.awt.*;
+import java.util.*;
+import java.util.List;
 
-/**
- * Created by FEngelbrektsson on 15/04/16.
- */
 public abstract class Movable {
 
     private double canvasPositionX;
     private double canvasPositionY;
     private double velocityX;
     private double velocityY;
+    private double speed;
+    private boolean moving;
+    private Direction direction;
+    private CharacterState characterState;
 
-    protected Movable() { }
+    private int previousGridPositionX;
+    private int previousGridPositionY;
+    private StaticTile[][] latestArenaTiles;
 
-    public Movable(double canvasPositionX, double canvasPositionY) {
+    public Movable(double canvasPositionX, double canvasPositionY, double speed) {
         this.canvasPositionX = canvasPositionX;
         this.canvasPositionY = canvasPositionY;
         this.velocityX = 0;
         this.velocityY = 0;
+        this.speed = speed;
+        this.moving = false;
+
+        this.previousGridPositionX = Utils.canvasToGridPosition(getCanvasPositionX());
+        this.previousGridPositionY = Utils.canvasToGridPosition(getCanvasPositionY());
+
+        this.moving = false;
+
+        this.characterState = CharacterState.IDLE;
     }
 
     public void updatePosition() {
         this.canvasPositionX += this.velocityX;
         this.canvasPositionY += this.velocityY;
+        if (characterState == CharacterState.WALK) {
+            stopOnTileIfNeeded();
+        }
+        moving = (getVelocityX() != 0 || getVelocityY() != 0);
+
+        if (characterState == CharacterState.WALK) {
+            stopOnTileIfNeeded();
+        }
+
     }
 
-    // GET N SETS
+    private void stop(int newGridPositionX, int newGridPositionY) {
+
+        characterState = CharacterState.IDLE;
+        setVelocity(0, 0);
+        setCanvasPosition(Utils.gridToCanvasPosition(newGridPositionX), Utils.gridToCanvasPosition(newGridPositionY));
+        setPreviousGridPositionX(newGridPositionX);
+        setPreviousGridPositionY(newGridPositionY);
+
+        List<String> input = ListenerController.getInstance().getInput();
+        if (input.size() > 0 && (input.contains("UP") || input.contains("DOWN") || input.contains("LEFT") || input.contains("RIGHT"))) {
+            move(input.get(input.size()-1), this.latestArenaTiles);
+        }
+    }
+
+    private void stopOnTileIfNeeded() {
+        int closestTilePositionX = Utils.canvasToGridPosition(getCanvasPositionX());
+        int closestTilePositionY = Utils.canvasToGridPosition(getCanvasPositionY());
+
+        double compareX = Math.abs(Utils.gridToCanvasPosition(closestTilePositionX) - getCanvasPositionX());
+        double compareY = Math.abs(Utils.gridToCanvasPosition(closestTilePositionY) - getCanvasPositionY());
+
+        boolean closeToPosition = (compareX <= this.getSpeed() / 2) && (compareY <= this.getSpeed() / 2)
+                && (moving || (closestTilePositionX != getPreviousGridPositionX() || closestTilePositionY != getPreviousGridPositionY()));
+
+        if(closeToPosition) {
+            stop(closestTilePositionX, closestTilePositionY);
+        }
+    }
 
     public void setVelocity(double velocityX, double velocityY) {
         this.velocityX = velocityX;
@@ -42,6 +96,130 @@ public abstract class Movable {
     }
     public double getVelocityY() {
         return this.velocityY;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public double getSpeed() {
+        return this.speed;
+    }
+
+    public void death() {
+        if (characterState == CharacterState.IDLE) {
+            characterState = CharacterState.DEATH;
+        }
+    }
+
+    public void spawn() {
+        if (characterState == CharacterState.IDLE) {
+            characterState = CharacterState.SPAWN;
+        }
+    }
+
+    public void move(String key, StaticTile[][] arenaTiles) {
+        this.latestArenaTiles = arenaTiles;
+        if (characterState != CharacterState.WALK || oppositeDirection(key)) {
+            switch (key) {
+                case "UP":
+                    moveUp();
+                    break;
+                case "LEFT":
+                    moveLeft();
+                    break;
+                case "DOWN":
+                    moveDown();
+                    break;
+                case "RIGHT":
+                    moveRight();
+                    break;
+            }
+            if (getVelocityY() != 0 || getVelocityX() != 0) {
+                characterState = CharacterState.WALK;
+            }
+        }
+    }
+
+    public void moveUp() {
+        setDirection(Direction.UP);
+        if (allowMove(0, -1) && getCanvasPositionY() > 0) {
+            setVelocity(0, -this.getSpeed());
+        }
+    }
+    public void moveDown() {
+        setDirection(Direction.DOWN);
+        if (allowMove(0, 1) && getCanvasPositionY() < Utils.gridToCanvasPosition(Constants.DEFAULT_GRID_HEIGHT-1)) {
+            setVelocity(0, this.getSpeed());
+        }
+    }
+    public void moveLeft() {
+        setDirection(Direction.LEFT);
+        if (allowMove(-1, 0) && getCanvasPositionX() > 0) {
+            setVelocity(-this.getSpeed(), 0);
+        }
+    }
+    public void moveRight() {
+        setDirection(Direction.RIGHT);
+        if (allowMove(1, 0) && getCanvasPositionX() < Utils.gridToCanvasPosition(Constants.DEFAULT_GRID_WIDTH-1)) {
+            setVelocity(this.getSpeed(), 0);
+        }
+    }
+
+    private boolean allowMove(int gridDirectionX, int gridDirectionY) {
+        int nextGridPositionX = Math.min(Math.max(getPreviousGridPositionX() + gridDirectionX, 0), Constants.DEFAULT_GRID_WIDTH-1);
+        int nextGridPositionY = Math.min(Math.max(getPreviousGridPositionY() + gridDirectionY, 0), Constants.DEFAULT_GRID_HEIGHT-1);
+        StaticTile nextTile = this.latestArenaTiles[nextGridPositionX][nextGridPositionY];
+
+        return nextTile == null || !(nextTile instanceof Wall) && !(nextTile instanceof Bomb);
+    }
+
+    private boolean oppositeDirection(String key) {
+        return (key.equals("UP") && getDirection() == Direction.DOWN)
+                || (key.equals("DOWN") && getDirection() == Direction.UP)
+                || (key.equals("RIGHT") && getDirection() == Direction.LEFT)
+                || (key.equals("LEFT") && getDirection() == Direction.RIGHT);
+    }
+
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+
+    public boolean getMoving() {
+        return this.moving;
+    }
+
+    public void setMoving(boolean moving) {
+        this.moving = moving;
+    }
+
+    public CharacterState getState() {
+        return this.characterState;
+    }
+
+    public Point getGridPosition() {
+        return new Point(Utils.canvasToGridPosition(this.getCanvasPositionX(), this.getCanvasPositionY()));
+    }
+
+    public int getPreviousGridPositionX() {
+        return this.previousGridPositionX;
+    }
+
+    public int getPreviousGridPositionY() {
+        return this.previousGridPositionY;
+    }
+
+    public void setPreviousGridPositionX(int previousGridPositionX) {
+        this.previousGridPositionX = previousGridPositionX;
+    }
+
+    public void setPreviousGridPositionY(int previousGridPositionY) {
+        this.previousGridPositionY = previousGridPositionY;
     }
 
     public void setCanvasPosition(double canvasPositionX, double canvasPositionY) {
