1725a04c4288d898cf8f18c8e498c0e6e3e1cb34
ryggan <andreas@carlssonfoto.se>
2016-04-25 13:38:44 +0200

    Removed obsolete code in Enemy since its already in Movable

--- src/main/java/edu/chalmers/vaporwave/model/gameObjects/Enemy.java
+++ src/main/java/edu/chalmers/vaporwave/model/gameObjects/Enemy.java
@@ -5,39 +5,8 @@ import edu.chalmers.vaporwave.view.Sprite;
 
 import java.awt.*;
 
-/**
- * Created by bob on 2016-04-15.
- */
 public class Enemy extends Movable {
 
-    private Point currentGridPosition;
-    private Point previousGridPosition;
 
-    public void moveUp() {
-        previousGridPosition = currentGridPosition;
-        currentGridPosition.setLocation(previousGridPosition.getX(), previousGridPosition.getY() + 1);
-    }
-    public void moveDown() {
-        previousGridPosition = currentGridPosition;
-        currentGridPosition.setLocation(previousGridPosition.getX(), previousGridPosition.getY() - 1);
-    }
-    public void moveLeft() {
-        previousGridPosition = currentGridPosition;
-        currentGridPosition.setLocation(previousGridPosition.getX() - 1, previousGridPosition.getY());
-    }
-    public void moveRight() {
-        previousGridPosition = currentGridPosition;
-        currentGridPosition.setLocation(previousGridPosition.getX() + 1, previousGridPosition.getY());
-    }
-
-    public Point getPosition() {
-        return currentGridPosition;
-    }
-    public double getXPosition() {
-        return currentGridPosition.getX();
-    }
-    public double getYPosition() {
-        return currentGridPosition.getY();
-    }
 
 }
