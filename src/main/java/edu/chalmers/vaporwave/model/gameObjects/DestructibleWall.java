6d7dbb4e73fa051db2fbc051c28f37d530f1b6c7
ryggan <andreas@carlssonfoto.se>
2016-04-25 13:43:42 +0200

    Removing unnecessary wallDestroyed boolean in destructible wall

--- src/main/java/edu/chalmers/vaporwave/model/gameObjects/DestructibleWall.java
+++ src/main/java/edu/chalmers/vaporwave/model/gameObjects/DestructibleWall.java
@@ -1,29 +1,3 @@
 package edu.chalmers.vaporwave.model.gameObjects;
 
-import edu.chalmers.vaporwave.view.Sprite;
-
-import java.awt.*;
-
-/**
- * Created by FEngelbrektsson on 15/04/16.
- */
-public class DestructibleWall extends Wall {
-
-    private boolean isDestroyed;
-
-    public DestructibleWall(){
-        isDestroyed = false;
-    }
-
-    private void setDestroyed(){
-        isDestroyed = true;
-    }
-
-    private boolean getDestroyed(){
-        return isDestroyed;
-    }
-
-
-    /////needs spriteinfo, dynamic, cuz need to change when destroyed
-
-}
+public class DestructibleWall extends Wall { }
