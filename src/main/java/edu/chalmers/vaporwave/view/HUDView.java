ad6fdb606306ecb822cc88a95de7ff8ae5d0a3cc
Lina <bllina@student.chalmers.se>
2016-04-28 13:28:49 +0200

    Added healthbaranimation

--- src/main/java/edu/chalmers/vaporwave/view/HUDView.java
+++ src/main/java/edu/chalmers/vaporwave/view/HUDView.java
@@ -16,11 +16,16 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.WritableImage;
 
 
 import java.awt.*;
 
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class HUDView {
 
@@ -32,23 +37,41 @@ public class HUDView {
     private MenuButtonView healthbarSprite;
 
     private ImageView healthBarEmpty;
+    private ImageView healthBarFilled;
     private Group root;
     private Canvas hudCanvas;
     private GraphicsContext hudGC;
 
+    private PixelReader reader;
+    private WritableImage newImage;
 
     public HUDView(Group root){
 
+
+
         this.root = root;
         this.healthBarEmpty = new ImageView(new Image("images/healthbarempty.png"));
+
+        Image healthbar=new Image("images/healthbarfilled.png");
+        reader = healthbar.getPixelReader();
+        newImage = new WritableImage(reader, 0, 0, 300,17 );
+
+        this.healthBarFilled = new ImageView(newImage);
+
         this.healthBarEmpty.setLayoutX(140);
         this.healthBarEmpty.setLayoutY(-10);
+        this.healthBarFilled.setLayoutX(184);
+        this.healthBarFilled.setLayoutY(38);
+
 
         this.stats = new Label();
 
         hudCanvas = new Canvas(Constants.GAME_WIDTH + (Constants.DEFAULT_TILE_WIDTH * 4 * Constants.GAME_SCALE), ((Constants.GAME_HEIGHT + Constants.GRID_OFFSET_Y) * Constants.GAME_SCALE));
         this.root.getChildren().add(hudCanvas);
         this.root.getChildren().add(healthBarEmpty);
+        this.root.getChildren().add(healthBarFilled);
+
+
 
     }
 
@@ -56,11 +79,24 @@ public class HUDView {
         int printHealth = (int)health;
         int printSpeed = (int)(speed * 100);
 
+        updateHealthBar((int) ((health/100) * 300));
+
         stats.setText("Health: " + printHealth + "\nBombs: " + bombCount + "\nSpeed: " + printSpeed + "\nRange: " + range);
         stats.setLayoutX(920);
         stats.setLayoutY(152);
         this.root.getChildren().remove(stats);
         this.root.getChildren().add(stats);
+    }
+
+    public void updateHealthBar(int i){
+        newImage = new WritableImage(reader, 0, 0, i, 17);
+        ImageView healthBarFilledNew = new ImageView(newImage);
+        healthBarFilledNew.setLayoutX(184);
+        healthBarFilledNew.setLayoutY(38);
+        this.root.getChildren().remove(healthBarFilled);
+        this.root.getChildren().add(healthBarFilledNew);
+        this.healthBarFilled = healthBarFilledNew;
+
     }
 
 
