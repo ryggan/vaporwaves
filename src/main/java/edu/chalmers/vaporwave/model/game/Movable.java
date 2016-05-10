daa946960ff2d5602b63f9cf0c53040346fdf093
bob <robin.lilius@gmail.com>
2016-05-10 13:30:40 +0200

    Fixed spawn-walking bugg

--- src/main/java/edu/chalmers/vaporwave/model/game/Movable.java
+++ src/main/java/edu/chalmers/vaporwave/model/game/Movable.java
@@ -170,14 +170,14 @@ public abstract class Movable {
         invincibleTimer = invincibleDelay;
         flinchInvincible = true;
         setState(MovableState.FLINCH);
-        System.out.println("Flinch! "+getName());
+//        System.out.println("Flinch! "+getName());
     }
 
     public void death() {
         stop();
         setState(MovableState.DEATH);
         this.health=0;
-        System.out.println("Death! "+getName());
+//        System.out.println("Death! "+getName());
     }
 
     public void spawn(Point spawningPoint) {
@@ -186,13 +186,14 @@ public abstract class Movable {
         stopAtTile((int)spawningPoint.getX(), (int)spawningPoint.getY());
         setComingState(MovableState.SPAWN);
         this.health = Constants.DEFAULT_START_HEALTH;
-        System.out.println("Spawn! "+getName());
+//        System.out.println("Spawn! "+getName());
     }
 
     public void move(Direction direction, StaticTile[][] arenaTiles) {
         this.lastMove = direction;
         this.latestArenaTiles = arenaTiles;
-        if (direction != null && (movableState == MovableState.IDLE || (movableState == MovableState.WALK && oppositeDirection(direction)))) {
+        if (direction != null && this.comingState != MovableState.SPAWN &&
+                (movableState == MovableState.IDLE || (movableState == MovableState.WALK && oppositeDirection(direction)))) {
             switch (direction) {
                 case UP:
                     moveUp();
