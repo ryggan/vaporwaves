09de2c495449eacfab1cff73ce0697b8a784d5a9
bob <robin.lilius@gmail.com>
2016-05-10 08:57:06 +0200

    Moved PauseMenuSheet.css to css-directory

--- src/main/java/edu/chalmers/vaporwave/view/HUDView.java
+++ src/main/java/edu/chalmers/vaporwave/view/HUDView.java
@@ -1,17 +1,19 @@
 package edu.chalmers.vaporwave.view;
-;
+
 import edu.chalmers.vaporwave.util.Constants;
 import edu.chalmers.vaporwave.util.TimerModel;
 import javafx.scene.Group;
-import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
-import javafx.scene.canvas.Canvas;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.PixelReader;
 import javafx.scene.image.WritableImage;
 import javafx.scene.text.Font;
+
 import java.awt.*;
+
+;
 
 public class HUDView {
 
