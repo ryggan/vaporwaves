6aa2d091672ab45e6f488ec595918232eea62e73
ryggan <andreas@carlssonfoto.se>
2016-05-23 16:22:52 +0200

    Minor refactoring in CPUPlayer

--- src/main/java/edu/chalmers/vaporwave/model/game/CPUPlayer.java
+++ src/main/java/edu/chalmers/vaporwave/model/game/CPUPlayer.java
@@ -1,13 +1,31 @@
 package edu.chalmers.vaporwave.model.game;
 
+import com.sun.javafx.scene.traversal.Direction;
 import edu.chalmers.vaporwave.model.Player;
+
+import java.awt.*;
+import java.util.Set;
 
 public class CPUPlayer extends Player {
 
     private PlayerAI playerAI;
 
-    public CPUPlayer(int playerId, String playerName, PlayerAI playerAI) {
+    public CPUPlayer(int playerId, String playerName) {
         super(playerId, playerName);
+        this.playerAI = new PlayerAI() {
+            @Override
+            public boolean shouldPutBomb() {
+                return false;
+            }
+
+            @Override
+            public Direction getNextMove(Point enemyPosition, StaticTile[][] arenaTiles, Set<Enemy> enemies) {
+                return null;
+            }
+        };
+    }
+
+    public void setPlayerAI(PlayerAI playerAI) {
         this.playerAI = playerAI;
     }
 
