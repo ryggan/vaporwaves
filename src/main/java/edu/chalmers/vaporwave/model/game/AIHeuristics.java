af21897176b473823088c4fdf4c4a334640bc2f6
Felix Engelbrektsson <ugglan00@gmail.com>
2016-05-23 15:51:26 +0200

    Changes to evaluation

--- src/main/java/edu/chalmers/vaporwave/model/game/AIHeuristics.java
+++ src/main/java/edu/chalmers/vaporwave/model/game/AIHeuristics.java
@@ -77,23 +77,31 @@ public class AIHeuristics {
         //set up a meeting point in center
         //recursive(new Point(11,8), 80);
 
-        recursive(new Point(11,8), 200);
+        recursive(new Point(11,8), 200, 10);
 
         for(int i = 0; i < arenaTiles.length; i++) {
             for (int j = 0; j < arenaTiles[0].length; j++) {
                 if (arenaTiles[i][j] instanceof PowerUp) {
-                    recursive(new Point(i,j), 100);
+                    recursive(new Point(i,j), 100, 10);
                 }
             }
         }
 
         for (GameCharacter gameCharacter : gameCharacters) {
-            recursive(gameCharacter.getGridPosition(), 200);
+            recursive(gameCharacter.getGridPosition(), 200, 10);
             //decideHeuristicValue(heuristicMatrix, gameCharacter.getGridPosition(), 100);
         }
 
         for (Enemy enemy : enemies) {
-            recursive(enemy.getGridPosition(), 10);
+            recursive(enemy.getGridPosition(), 3, 1);
+        }
+
+        for(int i = 0; i < arenaTiles.length; i++) {
+            for (int j = 0; j < arenaTiles[0].length; j++) {
+                if (arenaTiles[i][j] instanceof Explosive) {
+                    recursive(new Point(i,j), 100, 10);
+                }
+            }
         }
 
         for(int i = 0; i < arenaTiles.length; i++) {
@@ -150,14 +158,14 @@ public class AIHeuristics {
         }
     }*/
 
-    public static void recursive(Point playerPosition, int startValue) {
+    public static void recursive(Point playerPosition, int startValue, int difference) {
         int x = playerPosition.x;
         int y = playerPosition.y;
         heuristicMatrix[x][y] = startValue;
-        recursiveLeft(x-1, y, startValue - 10);
-        recursiveRight(x+1, y, startValue - 10);
-        recursiveDown(x, y-1, startValue - 10);
-        recursiveUp(x, y+1, startValue - 10);
+        recursiveLeft(x-1, y, startValue - difference, difference);
+        recursiveRight(x+1, y, startValue - difference, difference);
+        recursiveDown(x, y-1, startValue - difference, difference);
+        recursiveUp(x, y+1, startValue - difference, difference);
 
 /*        recursiveSW(x-1, y-1, startValue - 30);
         recursiveSE(x+1, y-1, startValue - 30);
@@ -166,45 +174,45 @@ public class AIHeuristics {
 
     }
 
-    public static void recursiveLeft(int x, int y, int tileValue) {
-        if(x > 0 && tileValue > 10) {
+    public static void recursiveLeft(int x, int y, int tileValue, int difference) {
+        if(x > 0 && tileValue > difference) {
             heuristicMatrix[x][y] = tileValue;
-            recursiveLeft(x - 1, y, tileValue - 10);
+            recursiveLeft(x - 1, y, tileValue - difference, difference);
 /*            int z = x-1;
             recursiveNW(z-1, y+1, tileValue-15);
             recursiveSW(z-1, y-1, tileValue-15);*/
         }
     }
 
-    public static void recursiveUp(int x, int y, int tileValue) {
-        if(y < 14 && tileValue > 10) {
+    public static void recursiveUp(int x, int y, int tileValue, int difference) {
+        if(y < 14 && tileValue > difference) {
             heuristicMatrix[x][y] = tileValue;
-            recursiveUp(x, y + 1, tileValue - 10);
+            recursiveUp(x, y + 1, tileValue - difference, difference);
 
-            recursiveLeft(x-1,y, tileValue-10);
-            recursiveRight(x+1,y,tileValue-10);
+            recursiveLeft(x-1,y, tileValue-difference, difference);
+            recursiveRight(x+1,y,tileValue-difference, difference);
 /*            int z = y +1;
             recursiveNE(x+1, z+1, tileValue-15);
             recursiveNW(x-1, z+1, tileValue-15);*/
         }
     }
 
-    public static void recursiveRight(int x, int y, int tileValue) {
-        if(x < 20 && tileValue > 10) {
+    public static void recursiveRight(int x, int y, int tileValue, int difference) {
+        if(x < 20 && tileValue > difference) {
             heuristicMatrix[x][y] = tileValue;
-            recursiveRight(x + 1, y, tileValue - 10);
+            recursiveRight(x + 1, y, tileValue - difference, difference);
 /*            int z = x + 1;
             recursiveNE(z+1, y+1, tileValue-15);
             recursiveSE(z+1, y-1, tileValue-15);*/
         }
     }
 
-    public static void recursiveDown(int x, int y, int tileValue) {
+    public static void recursiveDown(int x, int y, int tileValue, int difference) {
         if(y > 0 && tileValue > 10) {
             heuristicMatrix[x][y] = tileValue;
-            recursiveLeft(x-1,y, tileValue-10);
-            recursiveRight(x+1,y,tileValue-10);
-            recursiveDown(x, y-1, tileValue - 10);
+            recursiveLeft(x-1,y, tileValue-difference, difference);
+            recursiveRight(x+1,y,tileValue-difference, difference);
+            recursiveDown(x, y-1, tileValue - difference, difference);
 
 
 /*            int z = y-1;
